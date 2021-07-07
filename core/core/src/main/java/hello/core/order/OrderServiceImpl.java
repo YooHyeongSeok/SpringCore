bfaccdd5566e2d6676dea32a4259ad55143e9aaf
YooHyungSeok <broseok@naver.com>
2021-07-07 22:09:40 +0900

    의존관계 4가지 주입방법

--- core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -14,11 +14,43 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    //생성자가 딱 1개만 있을때 생략 가능
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    /*
+        수정자 주입
+        private MemberRepository memberRepository;
+        private DiscountPolicy discountPolicy;
+
+        //주입할 대상이 없어도 동작 가능한 옵션
+        @Autowired(required = false)
+        public void setMemberRepository(MemberRepository memberRepository) {
+            this.memberRepository = memberRepository;
+        }
+        @Autowired
+        public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+            this.discountPolicy = discountPolicy;
+        }
+
+        //필드 주입
+        //외부에서 변경이 불가능하여 테스트하기 힘들다.
+        @Autowired
+        private MemberRepository memberRepository;
+        @Autowired
+        private DiscountPolicy discountPolicy;
+
+        //일반 메서드 주입
+        //일반적으로 잘 사용하지 않는다.
+        @Autowired
+        public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+             this.memberRepository = memberRepository;
+             this.discountPolicy = discountPolicy;
+         }
+    */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
