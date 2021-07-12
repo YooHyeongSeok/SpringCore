29d4d3238fdf2597c22a06904ed7105bf5ed0a3a
YooHyungSeok <broseok@naver.com>
2021-07-12 21:46:51 +0900

    @PostConstruct, @PreDestory사용

--- core/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -28,11 +31,13 @@ public class NetworkClient {
         System.out.println("close : " + url);
     }
 
+    @PostConstruct
     public void init() throws Exception {
         connect();
         call("초기화 연결메시지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         disConnect();
     }
