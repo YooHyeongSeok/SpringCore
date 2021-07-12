05a109c64d13687bc4bbf9356cf688f8e3ad302a
YooHyungSeok <broseok@naver.com>
2021-07-12 21:37:30 +0900

    빈 생명주기 예제

---
+++ core/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,32 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 url = " + url);
+        connect();
+        call("초기화 연결메시지");
+        this.url = url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+
+        System.out.println("connect : " + url);
+
+    }
+
+    public void call(String message) {
+        System.out.println("call :: " + url + " message :::" + message);
+    }
+
+    public void disConnect() {
+        System.out.println("close : " + url);
+    }
+
+}
